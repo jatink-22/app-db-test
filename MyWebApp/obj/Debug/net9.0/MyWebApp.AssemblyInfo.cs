e298a59da0200fe31f0e68338d355b299ff494da
Jatink-22 <kuchiyajatin6@gmail.com>
2025-03-08 22:22:32 +0000

    my-chages

--- MyWebApp/obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
+++ MyWebApp/obj/Debug/net9.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3b3300cbd0576819d5cc0f6c9ae4a7134b1732")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87c8f93be65928ec1b70831f11452aed5d7064c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
